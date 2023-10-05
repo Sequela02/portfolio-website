e7f7c65b5288cff89f578ce749bd619f7e9eeea8
Canitas <r3xed@outlook.es>
2023-10-04 20:30:39 -0700

    Mejora del HomePage y Style css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,20 +2,21 @@
 // src/components/Header.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../assets/styles/styles.css';
 function Header() {
     return (
-        <header className="bg-black bg-opacity-90 h-auto flex flex-col justify-center items-center">
+        <header className="header h-auto flex flex-col justify-center items-center">
 
-            <nav className="flex justify-between items-center p-4">
+
+        <nav className="flex justify-between items-center p-4">
                 <div className="flex space-x-4">
-                    <Link to="/" className="text-white hover:text-blue-400 transition duration-300">
+                    <Link to="/" className="text-white hover:text-yellow-400 transition duration-300">
                         Me
                     </Link>
-                    <Link to="/projects" className="text-white hover:text-blue-400 transition duration-300">
+                    <Link to="/projects" className="text-white hover:text-yellow-400 transition duration-300">
                         Projects
                     </Link>
-                    <Link to="/hire-me" className="text-white hover:text-blue-400 transition duration-300">
+                    <Link to="/hire-me" className="text-white hover:text-yellow-400 transition duration-300">
                         Hire Me
                     </Link>
                 </div>
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,45 +1,40 @@
 import Header from '../components/Header';
 import '../assets/styles/styles.css';
 
-
 function HomePage() {
     return (
-        <div className="background-gif">{/* added pt-16 for padding-top */}
-            <Header />
+        <div className="background-gif">
+            <Header className="header" />
 
-            <div className="flex flex-col justify-center items-center h-screen">
-                {/* Main Content */}
-                <div className="main-content p-4 bg-black bg-opacity-90 text-white rounded-xl">
-                    <div className="flex items-center">
-                        <img src="/favicon.ico" alt="Your Profile" className="w-32 h-32 rounded-full mr-4"/>
+            <main className="flex flex-row justify-center items-center h-screen">
+                <div className="main-content p-4 md:w-3/5 w-full rounded-xl text-gray-100 md:mr-4">
+                    <div className="flex items-center space-x-4">
+                        <img src="/favicon.ico" alt="Profile" className="w-32 h-32 rounded-full"/>
                         <div>
-                            <h1 className="text-4xl font-bold mb-2">Canitas</h1>
-                            <p className="text-xl">👨‍💻 Java Developer in the making | 📚 Computer Systems Engineering Student | 🎨 Exploring 3D Animation & 🕹️ Game Development |</p>
+                            <h1 className="text-4xl font-bold text-white mb-2 text-shadow">Canitas</h1>
+                            <p className="text-xl">
+                                👨‍💻 Java Developer in the making | 📚 Computer Systems Engineering Student |
+                                🎨 Exploring 3D Animation & 🕹️ Game Development |
+                            </p>
                         </div>
                     </div>
-
                     <h2 className="text-3xl font-bold my-4">Who am I?</h2>
-                    <p>Your detailed narrative here...</p>
+                    <p>aun no hay nada aqui...</p>
                 </div>
 
-                {/* Sidebar/Additional Info */}
-                <div className="sidebar p- bg-black bg-opacity-90 text-white rounded-xl">
+                <aside className="sidebar p-4 text-white md:w-2/5 w-full rounded-xl">
                     <h2 className="text-2xl font-bold mb-4">About me</h2>
                     <ul>
                         <li><strong>Age:</strong> 21</li>
+                        <li><strong>Age:</strong> Jul, 30</li>
                         <li><strong>Country:</strong> México</li>
-                        {/* ... add more details as required */}
+                        <li><strong>State:</strong> Sonora</li>
                     </ul>
-
-                    {/* If you have any call to action like sponsorship or hiring */}
-                    <button className="mt-4 px-6 py-2 bg-blue-600 text-white rounded-lg">Hire Me</button>
-                </div>
-            </div>
+                    <button className="mt-4 px-6 py-2 bg-blue-600 text-white rounded-lg">Paypal</button>
+                </aside>
+            </main>
         </div>
     );
 }
-
-
-
 
 export default HomePage;
