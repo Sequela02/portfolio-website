107c28de3cfd88b430a814e78e3c18625645cd91
Canitas <r3xed@outlook.es>
2023-10-09 19:46:38 -0700

    Mejora del HomePage y Style css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,44 +1,52 @@
-// Ejemplo: src/components/Header.jsx
-// src/components/Header.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/styles.css';
+import gitIcon from '../assets/images/git_icon.png';
+import xIcon from '../assets/images/twitter_icon.png';
+import linkedinIcon from '../assets/images/linkedin_icon.png';
+import twitchIcon from '../assets/images/twitch_icon.png';
+import ytIcon from '../assets/images/youtube_icon.png';
 function Header() {
     return (
-        <header className="header h-auto flex flex-col justify-center items-center">
+        <header className="header h-auto bg-gradient-to-r from-purple-600 via-blue-500 to-green-400 p-4 rounded-lg">
+            <div className="container mx-auto px-4">
+                <nav className="flex justify-between items-center w-full">
+                    <div className="flex space-x-4">
+                        <Link to="/" className="menu text-white hover:text-yellow-400 transition duration-300">
+                            Me
+                        </Link>
+                        <Link to="/projects" className="menu text-white hover:text-yellow-400 transition duration-300">
+                            Projects
+                        </Link>
+                        <Link to="/hire-me" className="menu text-white hover:text-yellow-400 transition duration-300">
+                            Hire Me
+                        </Link>
+                    </div>
 
+                    <div className="flex space-x-4">
+                        <a href="https://github.com/sequela02" target="_blank" rel="noopener noreferrer">
+                            <img src={gitIcon} alt="GitHub" className="w-8 h-8 hover:opacity-75 transition duration-300" />
+                        </a>
+                        <a href="https://twitter.com/sequela007" target="_blank" rel="noopener noreferrer">
+                            <img src={xIcon} alt="Twitter" className="w-8 h-8 hover:opacity-75 transition duration-300" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/ricardo-armenta-parra-b7066a201/" target="_blank" rel="noopener noreferrer">
+                            <img src={linkedinIcon} alt="LinkedIn" className="w-8 h-8 hover:opacity-75 transition duration-300" />
+                        </a>
+                        <a href="https://www.twitch.tv/canitas02" target="_blank" rel="noopener noreferrer">
+                            <img src={twitchIcon} alt="LinkedIn" className="w-8 h-8 hover:opacity-75 transition duration-300" />
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCP6JKowpTbHkHYmXZs0RoOA" target="_blank" rel="noopener noreferrer">
+                            <img src={ytIcon} alt="LinkedIn" className="w-8 h-8 hover:opacity-75 transition duration-300" />
+                        </a>
+                    </div>
 
-        <nav className="flex justify-between items-center p-4">
-                <div className="flex space-x-4">
-                    <Link to="/" className="text-white hover:text-yellow-400 transition duration-300">
-                        Me
-                    </Link>
-                    <Link to="/projects" className="text-white hover:text-yellow-400 transition duration-300">
-                        Projects
-                    </Link>
-                    <Link to="/hire-me" className="text-white hover:text-yellow-400 transition duration-300">
-                        Hire Me
-                    </Link>
-                </div>
-                <div className="flex space-x-4">
-                    <a href="path_to_GitHub" className="text-white hover:text-blue-400 transition duration-300">
-                        <i className="fab fa-github"></i>
-                    </a>
-                    <a href="path_to_Twitter" className="text-white hover:text-blue-400 transition duration-300">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                    {/* Add other social links similarly */}
-                </div>
-            </nav>
+                </nav>
 
-            {/* ...Rest of the Header content... */}
-
+            </div>
         </header>
     );
 }
 
+
 export default Header;
-
-
-
-
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,40 +1,66 @@
+/**
+ * HomePage Component
+ *
+ * This component represents the main landing page of the application.
+ *
+ * Props:
+ * - None
+ *
+ * State Variables:
+ * - None (as of now)
+ */
 import Header from '../components/Header';
 import '../assets/styles/styles.css';
 
 function HomePage() {
     return (
         <div className="background-gif">
-            <Header className="header" />
+            <div className="container mx-auto px-4">  {/* <-- Added container div */}
+                <Header className="header" />
+                <main className="flex flex-col md:flex-row justify-center items-start md:items-center h-auto md:h-screen">
+                    <div className="main-content p-4 my-2 md:my-0 md:w-3/5 w-full rounded-xl text-gray-100 md:mr-4">
 
-            <main className="flex flex-row justify-center items-center h-screen">
-                <div className="main-content p-4 md:w-3/5 w-full rounded-xl text-gray-100 md:mr-4">
-                    <div className="flex items-center space-x-4">
-                        <img src="/favicon.ico" alt="Profile" className="w-32 h-32 rounded-full"/>
+                        <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4">
+
+                        <img src="/favicon.ico" alt="Profile" className="w-24 h-24 md:w-32 md:h-32 rounded-full"/>
                         <div>
-                            <h1 className="text-4xl font-bold text-white mb-2 text-shadow">Canitas</h1>
+                            <h1 className="text-3xl md:text-4xl font-bold text-white mb-2 text-shadow">Canitas</h1>
+
                             <p className="text-xl">
-                                👨‍💻 Java Developer in the making | 📚 Computer Systems Engineering Student |
-                                🎨 Exploring 3D Animation & 🕹️ Game Development |
+                                👨‍💻 Java Developer in the making | 📚 Computer Systems Engineering Student | 🎨 Exploring 3D Animation & 🕹️ Game Development |
+                                {/* truncated for brevity */}
                             </p>
                         </div>
                     </div>
-                    <h2 className="text-3xl font-bold my-4">Who am I?</h2>
-                    <p>aun no hay nada aqui...</p>
+                    <section>
+                        <h2 className="text-3xl font-bold my-4">Who am I?</h2>
+                        My name is Ricardo, I'm a 21-year-old programmer from Mexico. I'm currently studying Computer Systems Engineering in university. I like programming games and working on solutions. 🎮💻
+                        I'm really passionate about programming, especially Java in the backend. ☕🤓 I also like watching movies and series, eating Mexican and Chinese food, 🇲🇽🇨🇳 and writing. ✍️ I hope to be a filmmaker one day. 🎥🎬
+                        In my free time, I like to hang out with my friends and family, 👫👭 play video games, 🎮🕹️👾 and watch movies. 🍿🎥🎞️ I'm also a big fan of music and I enjoy listening to different genres. 🎶🎧
+                        I'm excited to be a part of the programming community and I look forward to learning and growing as a programmer. 👨‍💻👩‍💻 I hope to one day be able to use my skills to make a positive impact on the world. 🌍🌎🌏
+                    </section>
                 </div>
+                <aside className="sidebar p-4 text-white md:ml-4 rounded-xl bg-gray-800">
+                    <section>
+                        <h2 className="text-2xl font-bold mb-4">About me</h2>
+                        <li><span className="emoji" aria-hidden="true">🎂</span><strong>Age:</strong> 21</li>
+                        <li><span className="emoji" aria-hidden="true">📅</span><strong>Birthdate:</strong> Jul, 30</li>
+                        <li><span className="emoji" aria-hidden="true">📍</span><strong>Country:</strong> México</li>
+                        <li><span className="emoji" aria-hidden="true">🏞️</span><strong>State:</strong> Sonora</li>
+                    </section>
 
-                <aside className="sidebar p-4 text-white md:w-2/5 w-full rounded-xl">
-                    <h2 className="text-2xl font-bold mb-4">About me</h2>
-                    <ul>
-                        <li><strong>Age:</strong> 21</li>
-                        <li><strong>Age:</strong> Jul, 30</li>
-                        <li><strong>Country:</strong> México</li>
-                        <li><strong>State:</strong> Sonora</li>
-                    </ul>
-                    <button className="mt-4 px-6 py-2 bg-blue-600 text-white rounded-lg">Paypal</button>
                 </aside>
-            </main>
+
+
+
+
+                </main>
+
+            </div>
+
         </div>
     );
 }
+
 
 export default HomePage;
