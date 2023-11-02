623278d8efcf15fd104aa4eb8f8e7c2d9989f123
Canitas <r3xed@outlook.es>
2023-11-02 16:47:50 -0700

    Listo para deploy

--- frontend/src/components/CVViewer.jsx
+++ frontend/src/components/CVViewer.jsx
@@ -6,8 +6,8 @@ import { pdfjs } from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function CVViewer({ file }) {
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
+    const [ setNumPages] = useState(null);
+    const [pageNumber] = useState(1);
 
     const onDocumentLoadSuccess = ({ numPages }) => {
         setNumPages(numPages);
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom'; // <-- Import NavLink at the top
+
 import '../assets/styles/styles.css';
 import { Link, useLocation } from 'react-router-dom';
 // Icons imports
