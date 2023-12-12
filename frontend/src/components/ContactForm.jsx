de7eacc628603d109d5dd7a9a18303f385a77cc3
Canitas <r3xed@outlook.es>
2023-12-12 16:17:14 -0700

    updated HireMePage.jsx and created hiremepage.css

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import '../assets/styles/header.css';
+import '../assets/styles/hiremepage.css';
+import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 function ContactForm() {
     return (
@@ -7,19 +10,20 @@ function ContactForm() {
             <h2 className="contact-title">Contáctame</h2>
 
             <div className="contact-detail">
+                <FontAwesomeIcon icon={faEnvelope} />
                 <label>Email:</label>
                 <a href="mailto:r3xed@outlook.e">r3xed@outlook.e</a>
             </div>
 
-
-
             <div className="contact-detail">
+                <FontAwesomeIcon icon={faLinkedin} />
                 <label>LinkedIn:</label>
                 <a href="https://www.linkedin.com/in/ricardo-armenta-parra-b7066a201/" target="_blank" rel="noreferrer">Perfil de LinkedIn</a>
             </div>
 
             {/* Otros métodos de contacto, como redes sociales o plataformas profesionales, también pueden ser incluidos aquí. */}
         </div>
+
     );
 }
 
--- frontend/src/pages/HireMePage.jsx
+++ frontend/src/pages/HireMePage.jsx
@@ -1,44 +1,65 @@
-// Imports
-import React, { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // AOS Styles
-import CVViewer from '../components/CVViewer';
-import ContactForm from '../components/ContactForm'; // Assuming you have footer.css contact form component
+import React from 'react';
+import ContactForm from '../components/ContactForm'; // Assuming this is your contact form component
+import '../assets/styles/hiremepage.css';
 
 
 function HireMePage() {
-    // Initialize AOS
-    useEffect(() => {
-        AOS.init();
-    }, []);
+
 
     return (
-        <div className="headerContainer mx-auto px-4 sm:px-6 lg:px-8 bg-fixed bg-no-repeat bg-cover text-gray-100">
-            {/* Main Content Area */}
-            <main className="flex flex-col justify-center items-center min-h-screen pt-10 pb-20">
-
-                {/* Hero Section */}
-                <section data-aos="fade-up" className="text-center max-w-2xl mx-auto mb-10 px-4">
-                    <h1 className="text-4xl sm:text-5xl font-bold mb-4">Let's create something together</h1>
-
-                </section>
+        <div className="hireMePage bg-fixed bg-no-repeat bg-cover text-gray-100 relative overflow-hidden">
 
 
-                {/* CV Viewer Section */}
-                <section data-aos="fade-left" className="mb-10 px-4 sm:px-6 lg:px-8">
-                    <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold ">My Resume</h2>
-                    <div className="f">
-                        <CVViewer file="/cv.pdf" />
+            <main className="flex flex-col justify-center items-center min-h-screen pt-10 pb-20 relative z-10">
+
+                {/* Hero Section with Canvas */}
+                <section className="hero-section text-center max-w-2xl mx-auto mb-10 px-4 relative">
+                    <div className="absolute top-0 left-0 w-full h-full z-0"></div>
+                    <div className="relative z-10">
+                        <h1 className="title text-4xl sm:text-5xl font-bold mb-4">Engage with Creative Minds</h1>
+                        <p className="description mb-4">Discover bespoke solutions crafted with precision and ingenuity. Let's bring your vision to life.</p>
                     </div>
                 </section>
-
-
-
+                {/* Project Presentation Section */}
+                <section className="project-section max-w-2xl mx-auto mb-10 px-4">
+                    <h2 className="text-2xl sm:text-3xl font-bold mb-4">Project Proposal Guidelines</h2>
+                    <p className="intro-text mb-4">
+                        If you're seeking a dedicated and skilled freelancer to bring your vision to life, you've come to the right place.
+                        I deliver custom solutions, from Minecraft mods to dynamic websites and tailored software. To get started, simply
+                        reach out with your project idea.
+                    </p>
+                    <div className="instructions mb-4">
+                        <p>Please provide a detailed project brief via Discord or Twitter, including a document with the following information:</p>
+                        <ul className="project-list">
+                            <li><strong>Project Name:</strong> A clear title that encapsulates your project's essence.</li>
+                            <li><strong>Key Features:</strong> An itemized list of functionalities and features that your project requires:</li>
+                            <ul className="features-list">
+                                <li>Feature #1: Detailed description of the feature.</li>
+                                <li>Feature #2: Detailed description of the feature.</li>
+                                <li>Feature #3: Detailed description of the feature.</li>
+                                {/* ... Add more features as needed */}
+                            </ul>
+                            <li><strong>Deadline:</strong> Your expected timeline and any references that could provide inspiration or direction.</li>
+                        </ul>
+                    </div>
+                    <p className="payment-info">
+                        A clear estimation of costs and delivery schedules will be provided once I have all the necessary details. Payments are
+                        typically divided into two milestones—50% to commence the project, and the remaining 50% upon final delivery. In the unlikely
+                        event that the software does not perform as required and a solution cannot be found, a refund can be discussed.
+                    </p>
+                </section>
 
                 {/* Contact Form Section */}
-                <section id="contact" data-aos="flip-up" className="w-full max-w-md mx-auto mb-10 px-4">
-                    <h2 className="text-2xl sm:text-3xl font-bold mb-3">Contact Me</h2>
+                <section id="contact" className="contact-form w-full max-w-md mx-auto mb-10 px-4">
+
                     <ContactForm />
+                </section>
+
+                {/* Sponsor Section */}
+                <section className="sponsor-section max-w-2xl mx-auto mb-10 px-4">
+                    <h2 className="text-2xl sm:text-3xl font-bold mb-4">Support Open Source</h2>
+                    <p>Your contributions help support my open-source projects and the broader community.</p>
+                    {/* Placeholder for PayPal integration */}
                 </section>
 
             </main>
