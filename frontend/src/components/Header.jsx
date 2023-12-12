48667fce6cdd6dff72233473d23ae91b48b6620b
Canitas <r3xed@outlook.es>
2023-12-11 21:05:00 -0700

    updated Header.jsx and header.css to match the website theme

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,53 +3,77 @@ import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faTwitter, faLinkedin, faTwitch, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import '../assets/styles/header.css';
-/**
- * Renders navigation links.
- *
- * @param {Object} props - The component props.
- * @param {Array} props.navLinks - An array of navigation links.
- * @param {string} props.currentPath - The current path.
- * @returns {JSX.Element} A div containing navigation links.
- */
-const NavigationLinks = ({ navLinks, currentPath }) => (
-    <div className="flex items-center justify-start space-x-4">
-        {navLinks.map(({ to, text }) => (
-            <Link
-                key={to}
-                to={to}
-                className={`px-3 py-2 text-lg ${currentPath === to ? ' font-semibold' : 'text-white'} transition duration-300 neon-text`}
-            >
-                {text}
-            </Link>
-        ))}
-    </div>
-);
+const NavLink = ({ to, text }) => {
+    const location = useLocation();
+    const isActive = location.pathname === to;
+
+    return (
+        <Link
+            to={to}
+            className={`nav-link ${isActive ? 'active' : ''}`}
+        >
+            {text}
+        </Link>
+    );
+};
+
+// Main component to render navigation links
+const NavigationLinks = ({ navLinks }) => {
+    return (
+        <div className="nav-container">
+            {navLinks.map(({ to, text }) => (
+                <NavLink
+                    key={to}
+                    to={to}
+                    text={text}
+                />
+            ))}
+        </div>
+    );
+};
+
 
 /**
  * Renders social media links.
  *
  * @returns {JSX.Element} A div containing social media icons.
  */
+/**
+ * SocialLinks - A component to display social media icons with interactive animations.
+ *
+ * This component renders a set of social media icons (GitHub, Twitter, LinkedIn, Twitch, and YouTube).
+ * Each icon is wrapped in an anchor tag that links to the respective social media page.
+ * Icons have a glitch effect on hover (applied via the 'glitch' class).
+ */
 const SocialLinks = () => (
     <div className="flex items-center justify-center md:justify-end space-x-4">
-        {/* Each icon has a neon effect applied through the social-icon class */}
-        <a href="https://github.com/sequela02" target="_blank" rel="noopener noreferrer" className="hover:scale-125 transition-transform duration-300 social-icon">
-            <FontAwesomeIcon icon={faGithub} className="w-8 h-8 md:w-10 md:h-10" />
+        {/* GitHub Icon */}
+        <a href="https://github.com/sequela02" target="_blank" rel="noopener noreferrer" className="glitch">
+            <FontAwesomeIcon icon={faGithub} className="icon" />
         </a>
-        <a href="https://twitter.com/sequela007" target="_blank" rel="noopener noreferrer" className="hover:scale-125 transition-transform duration-300 social-icon">
-            <FontAwesomeIcon icon={faTwitter} className="w-8 h-8 md:w-10 md:h-10" />
+
+        {/* Twitter Icon */}
+        <a href="https://twitter.com/sequela007" target="_blank" rel="noopener noreferrer" className="glitch">
+            <FontAwesomeIcon icon={faTwitter} className="icon" />
         </a>
-        <a href="https://www.linkedin.com/in/ricardo-armenta-parra-b7066a201/" target="_blank" rel="noopener noreferrer" className="hover:scale-125 transition-transform duration-300 social-icon">
-            <FontAwesomeIcon icon={faLinkedin} className="w-8 h-8 md:w-10 md:h-10" />
+
+        {/* LinkedIn Icon */}
+        <a href="https://www.linkedin.com/in/ricardo-armenta-parra-b7066a201/" target="_blank" rel="noopener noreferrer" className="glitch">
+            <FontAwesomeIcon icon={faLinkedin} className="icon" />
         </a>
-        <a href="https://www.twitch.tv/canitas02" target="_blank" rel="noopener noreferrer" className="hover:scale-125 transition-transform duration-300 social-icon">
-            <FontAwesomeIcon icon={faTwitch} className="w-8 h-8 md:w-10 md:h-10" />
+
+        {/* Twitch Icon */}
+        <a href="https://www.twitch.tv/canitas02" target="_blank" rel="noopener noreferrer" className="glitch">
+            <FontAwesomeIcon icon={faTwitch} className="icon" />
         </a>
-        <a href="https://www.youtube.com/channel/UCP6JKowpTbHkHYmXZs0RoOA" target="_blank" rel="noopener noreferrer" className="hover:scale-125 transition-transform duration-300 social-icon">
-            <FontAwesomeIcon icon={faYoutube} className="w-8 h-8 md:w-10 md:h-10" />
+
+        {/* YouTube Icon */}
+        <a href="https://www.youtube.com/channel/UCP6JKowpTbHkHYmXZs0RoOA" target="_blank" rel="noopener noreferrer" className="glitch">
+            <FontAwesomeIcon icon={faYoutube} className="icon" />
         </a>
     </div>
 );
+
 
 /**
  * Header component that renders the navigation and social links.
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,16 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
     extend: {
-      colors: {
-        cyberPink: '#ff007a',  // Neon Pink
-        cyberBlue: '#00f0ff',  // Neon Blue
-        cyberGreen: '#0aff01', // Neon Green
-        cyberDark: '#0d0d0d',  // Dark Background
-        // Add more as needed
-      },
-      fontFamily: {
-        cyber: ['Orbitron', 'sans-serif'],
-      },
+
+
+
     },
   },
   plugins: [],
