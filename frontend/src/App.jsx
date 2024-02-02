f8732456baa427fd7c0cc3646080493c6f27e42c
Canitas <r3xed@outlook.es>
2024-02-02 01:49:39 -0700

    Major update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@
  * @module App
  */
 
-import React, { lazy, Suspense } from 'react';
+import React, { lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -33,14 +33,14 @@ function App() {
                 <div className="flex flex-col min-h-screen">
                     <main className="flex-grow">
                         {/* Use Suspense to handle the lazy loading of the different pages */}
-                        <Suspense fallback={<div>Loading...</div>}>
+
                             <Routes>
                                 <Route path="/" element={<HomePage />} />
                                 <Route path="/portfolio" element={<ProjectsPage />} />
                                 <Route path="/hire-me" element={<HireMePage />} />
                                 <Route path="*" element={<NotFoundPage />} /> {/* Catch-all route for 404 page */}
                             </Routes>
-                        </Suspense>
+
                     </main>
                 </div>
                 <Footer />
