16f8b5eea34be162311607ba727edb6db5ab1a1d
Canitas <r3xed@outlook.es>
2024-02-02 10:22:17 -0700

    redirect fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@
  * @module App
  */
 
-import React, { lazy } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
